688f4b92848823b16d114b7dbeafe4e4f55eb58b
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-04-14 23:16:39 -0700

    Added NeighborhoodService to read and return neighbourhoods.json data

---
+++ src/Services/NeighbourhoodService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ContosoCrafts.WebSite.Services
+{
+    public class NeighbourhoodService
+    {
+        public NeighbourhoodService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string NeighbourhoodFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "neighbourhoods.json");
+
+        public IEnumerable<Neighbourhood> GetNeighbourhoods()
+        {
+            using var jsonFileReader = File.OpenText(NeighbourhoodFileName);
+            return JsonSerializer.Deserialize<Neighbourhood[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+        }
+    }
+}
