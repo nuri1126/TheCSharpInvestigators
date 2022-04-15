9917bd8e33d0cdf3e29b1fac7b2a69119ef6dd89
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-04-14 23:15:32 -0700

    Added Neighbourhood.cs model for the neighbourhoods.json file

---
+++ src/Models/Neighbourhood.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public class Neighbourhood
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string[] Comments { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Neighbourhood>(this);
+    }
+}
