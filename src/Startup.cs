8b2678a7525939d0d07b79ed698a0f477a8bc445
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-04-15 00:18:04 -0700

    Refactored spelling from neighbourhood to neighborhood

--- src/Startup.cs
+++ src/Startup.cs
@@ -29,7 +29,7 @@ namespace ContosoCrafts.WebSite
             services.AddHttpClient();
             services.AddControllers();
             services.AddTransient<JsonFileProductService>();
-            services.AddTransient<NeighbourhoodService>();
+            services.AddTransient<NeighborhoodService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
