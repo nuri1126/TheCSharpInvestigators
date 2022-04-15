ca8037b40fbd4c0e6f17801e81c665833398db78
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-04-15 00:15:00 -0700

    Added file NeighborhoodsController, Created api/neighborhoods endpoint

---
+++ src/Controllers/NeighborhoodsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NeighborhoodsController : ControllerBase
+    {
+        public NeighborhoodsController(NeighborhoodService neighborhoodService)
+        {
+            this.NeighborhoodService = neighborhoodService;
+        }
+        
+        public NeighborhoodService NeighborhoodService { get; }
+
+        [HttpGet]
+        public IEnumerable<Neighborhood> Get()
+        {
+            return NeighborhoodService.GetNeighborhoods();
+        }
+    }
+}
