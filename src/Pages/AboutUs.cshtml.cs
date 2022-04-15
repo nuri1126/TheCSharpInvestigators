8e983c2a8c227517b96cbe960737df0e49b6a89b
Zi Wang <80916549+zwang4-code@users.noreply.github.com>
2022-04-15 13:15:45 -0700

    ADD: About Us Link on Homepage ADD: A new About Us Page

---
+++ src/Pages/AboutUs.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    public class AboutUsModel : PageModel
+    {
+        private readonly ILogger<AboutUsModel> _logger;
+
+        public AboutUsModel(ILogger<AboutUsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
