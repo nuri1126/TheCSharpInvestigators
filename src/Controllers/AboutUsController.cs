c33521c62d1717004ee6a7346568fb2c0b5de32e
Zi Wang <80916549+zwang4-code@users.noreply.github.com>
2022-04-19 00:12:00 -0700

    Added AboutUsController.cs to /Controllers folder

---
+++ src/Controllers/AboutUsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LetsGoSEA.WebSite.Models;
+using LetsGoSEA.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LetsGoSEA.WebSite.Controllers
+{
+    /// <summary>
+    /// About Us Page Controller
+    /// </summary>
+    [Route("[controller]")]
+    [ApiController]
+    public class AboutUsController : Controller
+    {
+        public AboutUsController(AboutUsService aboutUsService)
+        {
+            this.aboutUsService = aboutUsService;
+        }
+
+        public AboutUsService aboutUsService { get; }
+
+        // Route: /AboutUs
+        // Returns a View with all the team member info
+        [HttpGet]
+        public IActionResult ShowAboutUsDetails()
+        {
+            var viewModel = aboutUsService.GetAboutUs();
+            return View(viewModel);
+        }
+
+
+    }
+}
