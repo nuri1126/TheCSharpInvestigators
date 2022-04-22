863b7ec2df25523cfbb55d8b9bbfea1746dee908
nuri1126 <nuri1126@users.noreply.github.com>
2022-04-22 01:38:25 -0700

    Added java script to make owl carousel on aboutus page work to src/wwwroot/js/site.js

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+// Owlcarousel
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        center: true,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+});
