e6d58412d355b27b279cd0c785e1c0ed0f5083cc
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-04-24 11:34:38 -0700

    Added Index.cshtml.Tests.cs with comments

---
+++ UnitTests/Pages/Index.cshtml.Tests.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using LetsGoSEA.WebSite.Pages;
+using LetsGoSEA.WebSite.Services;
+
+
+namespace UnitTests.Pages.Index
+{
+    /// <summary>
+    /// Unit test for Neighborhoods Index page 
+    /// </summary>
+    public class IndexTests
+    {
+        #region TestSetup
+
+        // IndexModel from Index.cshtml.cs for testing
+        public static IndexModel pageModel;
+
+        [SetUp] // Arrange 
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+
+            pageModel = new IndexModel(MockLoggerDirect, TestHelper.NeighborhoodServiceObj)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, pageModel.Neighborhoods.ToList().Any());
+        }
+        #endregion OnGet
+    }
+}
