1d065e8c9e205c5a5fc9c4de75eaf906b4ab8c41
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-04-24 20:14:33 -0700

    Added comments to NeighborhoodModel.cs

--- src/Models/NeighborhoodModel.cs
+++ src/Models/NeighborhoodModel.cs
@@ -2,16 +2,33 @@
 
 namespace LetsGoSEA.WebSite.Models
 {
+    /// <summary>
+    /// NeighborhoodModel represents a single neighborhoods and its characteristics
+    /// </summary>
     public class NeighborhoodModel
     {
+        // Primary key Id
         public int Id { get; set; } = 0;
+
+        // Neighborhood name, eg "West Seattle" 
         public string Name { get; set; } = "Default";
+
+        // Image of the neighborhood 
         public string Image { get; set; } = "Default";
+
+        // City 
         public string City { get; set; } = "Default";
+
+        // State
         public string State { get; set; } = "Default";
+
+        // Short description of the neighborhood
         public string ShortDesc { get; set; } = "Default";
+
+        // Array of comments from registered users 
         public string[] Comments { get; set; } =  {"Default"};
 
-    public override string ToString() => JsonSerializer.Serialize<NeighborhoodModel>(this);
+        // Converts Model to JSON
+        public override string ToString() => JsonSerializer.Serialize<NeighborhoodModel>(this);
     }
 }
