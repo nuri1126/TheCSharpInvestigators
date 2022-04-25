98c574bcd8f998ec25f94c1c4ce6844cf50aaeed
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-04-24 20:49:00 -0700

    Added comments to Pages/Index.cs.html.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,32 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
 using LetsGoSEA.WebSite.Models;
 using LetsGoSEA.WebSite.Services;
 
 namespace LetsGoSEA.WebSite.Pages
 {
-    /// <summary> 
-    /// Nuli Bang
     /// <summary>
-    /// /// <summary> 
-    /// Nirmalya Ghosh
-    /// <summary> 
-    /// <summary>
-    /// Zi Wang
-    /// <summary>
-    /// <summary>
-    /// Evan Marshall
+    /// Index Page Model for the homepage 
     /// </summary>
     public class IndexModel : PageModel
     {
+        // Logger property 
         private readonly ILogger<IndexModel> _logger;
+
+        // Instance of Neighborhood Service for dependency injection
         private readonly NeighborhoodService _neighborhoodService;
+
+        // Holds IEnumerable list of Neighborhoods 
         public IEnumerable<NeighborhoodModel> Neighborhoods { get; private set; }
 
+        // Creates a logger which uses a log category equal to the name of the model.
         public IndexModel(ILogger<IndexModel> logger,
             NeighborhoodService neighborhoodService)
         {
@@ -34,9 +28,8 @@ namespace LetsGoSEA.WebSite.Pages
             _neighborhoodService = neighborhoodService;
         }
 
-        // public IEnumerable<ProductModel> Products { get; private set; }
-        
-
+        // Returns a list of Neighborhoods to the Razor page Index.cshtml using 
+        // the Neighborhood Service
         public void OnGet()
         {
             Neighborhoods = _neighborhoodService.GetNeighborhoods();
