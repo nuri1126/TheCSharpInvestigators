a7d2b7ad55629053d4aa1d446325fae24392edb9
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-04-24 20:11:55 -0700

    Added comments to AboutUsModel.cs

--- src/Models/AboutUsModel.cs
+++ src/Models/AboutUsModel.cs
@@ -3,16 +3,26 @@
 namespace LetsGoSEA.WebSite.Models
 {
     /// <summary>
-    /// About Us Object (represent each team member)
+    /// AboutUsModel represents an individual developer profile
     /// </summary>
     public class AboutUsModel
     {
+        // Primary key Id
         public int Id { get; set; } = 0;
+
+        // Developer name
         public string Name { get; set; } = "Default";
+
+        // LinkedIn web address to developer profile
         public string LinkedIn { get; set; } = "Default";
+
+        // Developer photo 
         public string Image { get; set; } = "Default";
+
+        // Short bio 
         public string Bio { get; set; } = "Default";
 
+        // Converts Model to JSON
         public override string ToString() => JsonSerializer.Serialize<AboutUsModel>(this);
     }
 }
