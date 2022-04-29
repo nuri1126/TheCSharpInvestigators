52f7c03eb6a0bb47cec3fa6d335708ad6312f5bc
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-04-29 07:05:38 -0700

    Added LetsGoSEAWebSiteContext.cs

---
+++ src/Data/LetsGoSEAWebSiteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LetsGoSEA.WebSite.Models;
+
+namespace LetsGoSEA.WebSite.Data
+{
+    public class LetsGoSEAWebSiteContext : DbContext
+    {
+        public LetsGoSEAWebSiteContext (DbContextOptions<LetsGoSEAWebSiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LetsGoSEA.WebSite.Models.NeighborhoodModel> NeighborhoodModel { get; set; }
+    }
+}
