23b112627134b90f3cd55b9a588874c767cab5a2
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-04-29 07:08:07 -0700

    Added Create.cshtml.cs

---
+++ src/Views/Neighborhoods/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using LetsGoSEA.WebSite.Data;
+using LetsGoSEA.WebSite.Models;
+
+namespace LetsGoSEA.WebSite.Views.Neighborhoods
+{
+    public class CreateModel : PageModel
+    {
+        private readonly LetsGoSEA.WebSite.Data.LetsGoSEAWebSiteContext _context;
+
+        public CreateModel(LetsGoSEA.WebSite.Data.LetsGoSEAWebSiteContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public NeighborhoodModel NeighborhoodModel { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.NeighborhoodModel.Add(NeighborhoodModel);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
