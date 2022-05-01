2e3e1a63cfe15877236eb4b4b230231e5bb33a29
nuri1126 <nuri1126@users.noreply.github.com>
2022-05-01 15:26:43 -0700

    Added initial code to Read.cshtml.cs

--- src/Pages/Neighborhood/Read.cshtml.cs
+++ src/Pages/Neighborhood/Read.cshtml.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using LetsGoSEA.WebSite.Models;
+using LetsGoSEA.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LetsGoSEA.WebSite.Pages.Neighborhood
 {
     public class Read : PageModel
     {
-        public void OnGet()
+        private NeighborhoodService NeighborhoodService { get; }
+
+        public Read(NeighborhoodService neighborhoodService)
         {
-            
+            NeighborhoodService = neighborhoodService;
+        }
+
+        public NeighborhoodModel Neighborhood;
+
+        public void OnGet(int id)
+        {
+            Neighborhood = NeighborhoodService.GetNeighborhoodById(id);
         }
     }
 }
