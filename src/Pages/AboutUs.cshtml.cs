ce7081946ad18d06e6471e845d6121401590e914
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-01 14:03:26 -0700

    Added AboutUs.cshtml.cs

---
+++ src/Pages/AboutUs.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using LetsGoSEA.WebSite.Models;
+using LetsGoSEA.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LetsGoSEA.WebSite.Pages
+{
+    public class AboutUs : PageModel
+    {
+        
+        public AboutUsService AboutUsService { get; }
+
+        public AboutUs(AboutUsService aboutUsService)
+        {
+            AboutUsService = aboutUsService;
+        }
+
+        public IEnumerable<AboutUsModel> Members;
+
+        public void OnGet()
+        {
+            Members = AboutUsService.GetAboutUs();
+        }
+    }
+}
