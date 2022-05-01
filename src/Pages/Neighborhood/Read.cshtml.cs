fb0658d24f5d801126c19c91ea1b6c3a73ed26aa
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-01 14:20:33 -0700

    Creating initial Read.cshtml.cs

---
+++ src/Pages/Neighborhood/Read.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LetsGoSEA.WebSite.Pages.Neighborhood
+{
+    public class Read : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
