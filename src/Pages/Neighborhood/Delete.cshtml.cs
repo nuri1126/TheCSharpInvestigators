dae7537885aafbc65b96a97c05203cc3a566a2ca
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-01 14:18:26 -0700

    Creating initial Delete.cshtml.cs

---
+++ src/Pages/Neighborhood/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LetsGoSEA.WebSite.Pages.Neighborhood
+{
+    public class Delete : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
