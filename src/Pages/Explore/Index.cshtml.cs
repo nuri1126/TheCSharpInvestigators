100c8eabe197a7ab451b4138b94f46d89b59b152
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-01 12:07:54 -0700

    Added NeighborhoodService to Index.cshtml.cs

---
+++ src/Pages/Explore/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using LetsGoSEA.WebSite.Models;
+using LetsGoSEA.WebSite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LetsGoSEA.WebSite.Pages.Explore
+{
+    public class Index : PageModel
+    {
+        private NeighborhoodService NeighborhoodService { get; }
+
+        public Index(NeighborhoodService neighborhoodService)
+        {
+            NeighborhoodService = neighborhoodService;
+        }
+
+        public IEnumerable<NeighborhoodModel> Neighborhoods { get; private set; }
+
+        public void OnGet()
+        {
+            Neighborhoods = NeighborhoodService.GetNeighborhoods();
+        }
+    }
+}
