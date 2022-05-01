140b10057dc6c34e0b6218a312327a7670afc0e0
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-01 11:58:56 -0700

    Updated methods in NeighborhoodController.cs

---
+++ src/Controllers/NeighborhoodController.cs
@@ -0,0 +1,50 @@
+﻿using LetsGoSEA.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using LetsGoSEA.WebSite.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace LetsGoSEA.WebSite.Controllers
+{
+    /// <summary>
+    /// Neighborhoods Controller 
+    /// </summary>
+    [Route("[controller]")]
+    [ApiController]
+    public class NeighborhoodController : Controller
+    {
+        // Seed controller with service and DB context
+        public NeighborhoodController(NeighborhoodService neighborhoodService)
+        {
+            //TODO: Add null context check here
+
+            this.NeighborhoodService = neighborhoodService;
+        }
+
+        // Controller's retrievable service property used for dependency injection
+        private NeighborhoodService NeighborhoodService { get; }
+
+
+        // Displays a view of the Create page
+        //[HttpGet("/Neighborhoods/Create")]
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("id,name,image,city,state,shortDesc")] NeighborhoodModel neighborhood)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(neighborhood);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(neighborhood));
+        //    }
+        //    return View(neighborhood);
+        //}
+    }
+}
