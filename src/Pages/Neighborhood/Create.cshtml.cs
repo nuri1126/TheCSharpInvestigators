dc7a57f6d30b938f5131d70b2cc61de1baa042c9
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-01 14:18:07 -0700

    Creating initial Create.cshtml.cs

---
+++ src/Pages/Neighborhood/Create.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LetsGoSEA.WebSite.Pages.Neighborhood
+{
+    public class Create : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
