d0c08fb8af998d331cbb26b54ee52099091133bf
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-01 14:20:52 -0700

    Creating initial Update.cshtml.cs

---
+++ src/Pages/Neighborhood/Update.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LetsGoSEA.WebSite.Pages.Neighborhood
+{
+    public class Update : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
