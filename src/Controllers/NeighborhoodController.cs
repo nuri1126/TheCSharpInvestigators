6bb9ae719b5d14e813b84ef9482dfc71cfdb3631
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-01 14:50:40 -0700

    Removed unneeded comments from NeighorhoodController.cs

--- src/Controllers/NeighborhoodController.cs
+++ src/Controllers/NeighborhoodController.cs
@@ -18,33 +18,10 @@ namespace LetsGoSEA.WebSite.Controllers
         // Seed controller with service and DB context
         public NeighborhoodController(NeighborhoodService neighborhoodService)
         {
-            //TODO: Add null context check here
-
             this.NeighborhoodService = neighborhoodService;
         }
 
         // Controller's retrievable service property used for dependency injection
         private NeighborhoodService NeighborhoodService { get; }
-
-
-        // Displays a view of the Create page
-        //[HttpGet("/Neighborhoods/Create")]
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("id,name,image,city,state,shortDesc")] NeighborhoodModel neighborhood)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(neighborhood);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(neighborhood));
-        //    }
-        //    return View(neighborhood);
-        //}
     }
 }
