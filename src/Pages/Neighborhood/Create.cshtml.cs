f5ea21941a4b2b66472911d4aeef70f3b43d43fe
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-02 09:07:20 -0700

    Updated CreateModel to align with class example in Create.cshtml.cs

--- src/Pages/Neighborhood/Create.cshtml.cs
+++ src/Pages/Neighborhood/Create.cshtml.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using LetsGoSEA.WebSite.Models;
+using LetsGoSEA.WebSite.Services;
 
 namespace LetsGoSEA.WebSite.Pages.Neighborhood
 {
-    public class Create : PageModel
+    /// <summary>
+    /// CreateModel for adding a new Neighborhood to NeighborhoodModel and JSON file
+    /// </summary>
+    public class CreateModel : PageModel
     {
-        public void OnGet()
+        // Data middle tier
+        public NeighborhoodService NeighborhoodService { get; }
+
+        /// <summary>
+        /// Defualt Constructor
+        /// </summary>
+        /// <param name="neighborhoodService"></param>
+        public CreateModel(NeighborhoodService neighborhoodService)
         {
-            
+            NeighborhoodService = neighborhoodService;
+        }
+
+        // The data to show
+        public NeighborhoodModel Neighborhood;
+
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id"></param>
+        public IActionResult OnGet()
+        {
+            Neighborhood = NeighborhoodService.CreateData();
+
+            return RedirectToPage("./Update", new { Id = Neighborhood.Id });
         }
     }
 }
