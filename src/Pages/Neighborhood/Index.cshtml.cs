3589df9fd8ab47331348663ed5b0a1a88cd9ba40
Zi Wang <80916549+zwang4-code@users.noreply.github.com>
2022-05-02 13:26:12 -0700

    Added comment to Index.cshtml.cs

--- src/Pages/Neighborhood/Index.cshtml.cs
+++ src/Pages/Neighborhood/Index.cshtml.cs
@@ -5,18 +5,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LetsGoSEA.WebSite.Pages.Neighborhood
 {
+    /// <summary>
+    /// Index Page Model for the Neighborhood Razor Page: will return all the data to show
+    /// </summary>
     public class Index : PageModel
     {
-
+        // Data Service
         private NeighborhoodService NeighborhoodService { get;  }
-        
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="neighborhoodService">an instance of data service to use</param>
         public Index(NeighborhoodService neighborhoodService)
         {
             NeighborhoodService = neighborhoodService;
         }
-        
+
+        // Collection of the Neighborhood Data
         public IEnumerable<NeighborhoodModel> Neighborhoods { get; private set; }
 
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
         public void OnGet()
         {
             Neighborhoods = NeighborhoodService.GetNeighborhoods();
