004f675afdcb6d0e6e4d1b504a172a421a89dd4a
Zi Wang <80916549+zwang4-code@users.noreply.github.com>
2022-05-02 13:17:06 -0700

    Added comment to Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,15 +3,26 @@ using Microsoft.Extensions.Logging;
 
 namespace LetsGoSEA.WebSite.Pages
 {
+    /// <summary>
+    /// Privacy Page Model for the Privacy Razor Page
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        // Logger property 
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// Creates a Privacy logger which uses a log category equal to the name of the model.
+        /// </summary>
+        /// <param name="logger">An instance of the built-in ILogger service to use</param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// TODO ...
+        /// </summary>
         public void OnGet()
         {
         }
