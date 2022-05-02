fcd25ef37782f88bf23500277bf4c6b0d9bebd59
Zi Wang <80916549+zwang4-code@users.noreply.github.com>
2022-05-02 13:27:09 -0700

    Added comments to Read.cshtml.cs

--- src/Pages/Neighborhood/Read.cshtml.cs
+++ src/Pages/Neighborhood/Read.cshtml.cs
@@ -5,17 +5,30 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LetsGoSEA.WebSite.Pages.Neighborhood
 {
+    /// <summary>
+    /// Read Page Model for the Read Razor Page: will return one neighborhood's data to show
+    /// </summary>
     public class Read : PageModel
     {
+        // Data middletier
         private NeighborhoodService NeighborhoodService { get; }
 
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="neighborhoodService">an instance of the data service to use</param>
         public Read(NeighborhoodService neighborhoodService)
         {
             NeighborhoodService = neighborhoodService;
         }
 
+        // The data to show
         public NeighborhoodModel Neighborhood;
 
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        /// <param name="id">id of the neighborhood to show</param>
         public void OnGet(int id)
         {
             Neighborhood = NeighborhoodService.GetNeighborhoodById(id);
