7ebd91f4f4c45e88e7a84d1ed036b7fc6b13dd8f
Zi Wang <80916549+zwang4-code@users.noreply.github.com>
2022-05-02 13:13:40 -0700

    Added comments back to AboutUs.cshtml.cs

--- src/Pages/AboutUs.cshtml.cs
+++ src/Pages/AboutUs.cshtml.cs
@@ -5,18 +5,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LetsGoSEA.WebSite.Pages
 {
+    /// <summary>
+    /// AboutUs Page Model for the AboutUs Razor Page
+    /// </summary>
     public class AboutUs : PageModel
     {
-        
+        // Data service used to get team member data
         public AboutUsService AboutUsService { get; }
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="aboutUsService">An instance of the data service to use</param>
         public AboutUs(AboutUsService aboutUsService)
         {
             AboutUsService = aboutUsService;
         }
 
+        // Holds IEnumerable list of team member objects
         public IEnumerable<AboutUsModel> Members;
 
+        /// <summary>
+        /// Returns a list of team member objects to the AboutUs Razor Page
+        /// </summary>
         public void OnGet()
         {
             Members = AboutUsService.GetAboutUs();
