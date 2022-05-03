f24e212d71aa3393b1aac80feb2d62be6417e889
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-02 22:53:03 -0700

    Creating initial Read.cshtml.Tests.cs

---
+++ UnitTests/Pages/Neighborhood/Read.cshtml.Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using LetsGoSEA.WebSite.Pages.Neighborhood;
+
+namespace UnitTests.Pages.Neighborhood
+{
+    public class Read
+    {
+        #region TestSetup
+        public static ReadModel PageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            PageModel = new ReadModel(TestHelper.NeighborhoodServiceObj);
+        }
+
+        #endregion TestSetup
+
+
+        #region OnGet
+
+        [Test]
+        public void OnGet_Something()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+
+
+        #endregion OnGet
+    }
+}
