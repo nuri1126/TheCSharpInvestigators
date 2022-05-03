b5c2290809485f52d6676ad78c5a372a8a1aae17
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-02 22:53:20 -0700

    Creating initial Update.cshtml.Tests.cs

---
+++ UnitTests/Pages/Neighborhood/Update.cshtml.Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using LetsGoSEA.WebSite.Pages.Neighborhood;
+
+namespace UnitTests.Pages.Neighborhood
+{
+    public class Update
+    {
+        #region TestSetup
+        public static UpdateModel PageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            PageModel = new UpdateModel();
+        }
+
+        #endregion TestSetup
+
+
+        #region OnGet
+
+        [Test]
+        public void OnGet_Something()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+
+
+        #endregion OnGet
+    }
+}
