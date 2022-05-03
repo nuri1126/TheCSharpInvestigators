994f5890000f78fc8c79d185d524ef0dc415307b
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-02 22:52:45 -0700

    Creating initial Create.cshtml.Tests.cs

---
+++ UnitTests/Pages/Neighborhood/Create.cshtml.Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using LetsGoSEA.WebSite.Pages.Neighborhood;
+
+namespace UnitTests.Pages.Neighborhood
+{
+    public class Create
+    {
+        #region TestSetup
+        public static CreateModel PageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            PageModel = new CreateModel(TestHelper.NeighborhoodServiceObj);
+        }
+
+        #endregion TestSetup
+
+
+        #region OnGet
+
+        [Test]
+        public void OnGet_Something()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+
+
+        #endregion OnGet
+    }
+}
