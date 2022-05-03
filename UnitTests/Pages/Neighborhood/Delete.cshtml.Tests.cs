f58fb615d80e1c2d8ac5aa2c34fcfe6d23ced99d
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-02 22:52:53 -0700

    Creating initial Delete.cshtml.Tests.cs

---
+++ UnitTests/Pages/Neighborhood/Delete.cshtml.Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using LetsGoSEA.WebSite.Pages.Neighborhood;
+
+namespace UnitTests.Pages.Neighborhood
+{
+    public class DeleteModel
+    {
+        #region TestSetup
+        public static DeleteModel PageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            PageModel = new DeleteModel();
+        }
+
+        #endregion TestSetup
+
+
+        #region OnGet
+
+        [Test]
+        public void OnGet_Something()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+
+
+        #endregion OnGet
+    }
+}
