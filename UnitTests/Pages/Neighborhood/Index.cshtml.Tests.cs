9380d95c47ab8c241b197d50f479a5e1ab5d4b33
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-02 22:53:13 -0700

    Creating initial Index.cshtml.Tests.cs

---
+++ UnitTests/Pages/Neighborhood/Index.cshtml.Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using LetsGoSEA.WebSite.Pages.Neighborhood;
+
+namespace UnitTests.Pages.Neighborhood
+{
+    public class IndexModel
+    {
+        #region TestSetup
+        public static IndexModel PageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            PageModel = new IndexModel();
+        }
+
+        #endregion TestSetup
+
+
+        #region OnGet
+
+        [Test]
+        public void OnGet_Something()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+
+
+        #endregion OnGet
+    }
+}
