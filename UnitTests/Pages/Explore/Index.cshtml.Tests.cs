8791048ceb85b2819716bce544a2f3cc63415fe9
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-05 20:40:06 -0700

    Added unit test for Index to be passing in Index.cshtml.Tests.cs

---
+++ UnitTests/Pages/Explore/Index.cshtml.Tests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using LetsGoSEA.WebSite.Pages.Explore;
+using NUnit.Framework;
+
+namespace UnitTests.Pages.Explore
+{
+    public class IndexTests
+    {
+        #region TestSetup
+
+        private static IndexModel _pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            _pageModel = new IndexModel(TestHelper.NeighborhoodServiceObj);
+        }
+
+        #endregion TestSetup
+        
+        #region OnGet
+
+        [Test]
+        public void OnGet_Valid_Should_Return_Neighborhoods()
+        {
+            // Arrange
+            
+            
+            // Act
+            _pageModel.OnGet();
+            
+            // Assert
+            Assert.AreEqual(true, _pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, _pageModel.Neighborhoods.ToList().Any());
+        }
+        
+        #endregion OnGet
+    }
+}
