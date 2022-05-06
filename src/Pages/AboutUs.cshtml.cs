81aa99737f24dd38bc28e233edd5bf1c6a5dc162
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-05 20:14:21 -0700

    Updated model in AboutUs.cshtml.cs

--- src/Pages/AboutUs.cshtml.cs
+++ src/Pages/AboutUs.cshtml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using LetsGoSEA.WebSite.Models;
 using LetsGoSEA.WebSite.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,22 +7,22 @@ namespace LetsGoSEA.WebSite.Pages
     /// <summary>
     /// AboutUs Page Model for the AboutUs Razor Page
     /// </summary>
-    public class AboutUs : PageModel
+    public class AboutUsModel : PageModel
     {
         // Data service used to get team member data
-        public AboutUsService AboutUsService { get; }
+        private AboutUsService AboutUsService { get; }
 
         /// <summary>
         /// Default constructor
         /// </summary>
         /// <param name="aboutUsService">An instance of the data service to use</param>
-        public AboutUs(AboutUsService aboutUsService)
+        public AboutUsModel(AboutUsService aboutUsService)
         {
             AboutUsService = aboutUsService;
         }
 
         // Holds IEnumerable list of team member objects
-        public IEnumerable<AboutUsModel> Members;
+        public IEnumerable<Models.AboutUsModel> Members { get; private set; }
 
         /// <summary>
         /// Returns a list of team member objects to the AboutUs Razor Page
