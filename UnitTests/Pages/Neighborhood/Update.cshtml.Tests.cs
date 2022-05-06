c5172405a2d95ede8c8c5d82897ee7bb55df9890
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-05 20:23:44 -0700

    Added unit tests to passing in Update.cshtml.Tests.cs

--- UnitTests/Pages/Neighborhood/Update.cshtml.Tests.cs
+++ UnitTests/Pages/Neighborhood/Update.cshtml.Tests.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System.Diagnostics;
+using LetsGoSEA.WebSite.Models;
 using NUnit.Framework;
 using LetsGoSEA.WebSite.Pages.Neighborhood;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UnitTests.Pages.Neighborhood
 {
@@ -21,16 +23,62 @@ namespace UnitTests.Pages.Neighborhood
         #region OnGet
 
         [Test]
-        public void OnGet_Something()
+        public void OnGet_Valid_Should_Return_Products()
         {
             // Arrange
             
             // Act
+            PageModel.OnGet(2);
             
             // Assert
+            Assert.AreEqual(true, PageModel.ModelState.IsValid);
+            Assert.AreEqual("Greenlake", PageModel.Neighborhood.Name);
+        }
+        #endregion OnGet
+        
+        #region OnPostAsync
+
+        [Test]
+        public void OnPostAsync_Valid_Should_Return_Products()
+        {
+            // Arrange
+            PageModel.Neighborhood = new NeighborhoodModel
+            {
+                Id = 2,
+                Name = "Greenwood",
+                ShortDesc = "Welcome to the green neighborhood of Greenwood"
+            };
+            
+            // Act
+            var result = PageModel.OnPost() as RedirectToPageResult;
+            
+            // Assert
+            Assert.AreEqual(true, PageModel.ModelState.IsValid);
+            Debug.Assert(result != null, nameof(result) + " != null");
+            Assert.AreEqual(true, result.PageName.Contains("Index"));
         }
 
-
-        #endregion OnGet
+        [Test]
+        public void OnPostAsync_InValid_Model_NotValid_Return_Page()
+        {
+            // Arrange
+            PageModel.Neighborhood = new NeighborhoodModel
+            {
+                Id = 666,
+                Name = "Invalid Name",
+                ShortDesc = "Invalid Desc"
+            };
+            
+            // Force an invalid error state
+            PageModel.ModelState.AddModelError("InvalidState", "Invalid Neighborhood state");
+            
+            // Act
+            var result = PageModel.OnPost() as ActionResult;
+            
+            // Assert
+            Assert.AreEqual(false, PageModel.ModelState.IsValid);
+        }
+        
+        #endregion OnPostAsync
     }
 }
