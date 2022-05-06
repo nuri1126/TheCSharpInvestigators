7ada3b6daba82a58d29c277fc7f8564c638423da
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-05 20:39:04 -0700

    Added AboutUs page unit tests to be passing in AboutUs.cshtml.Tests.cs

---
+++ UnitTests/Pages/AboutUs.cshtml.Tests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using LetsGoSEA.WebSite.Pages;
+using NUnit.Framework;
+
+namespace UnitTests.Pages
+{
+    public class AboutUsTests
+    {
+        #region TestSetup
+
+        private static AboutUsModel _pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            _pageModel = new AboutUsModel(TestHelper.AboutUsServiceObj);
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        [Test]
+        public void OnGet_Valid_Should_Return_Members()
+        {
+            // Arrange
+            
+            // Act
+            _pageModel.OnGet();
+            
+            // Assert
+            Assert.AreEqual(true, _pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, _pageModel.Members.ToList().Any());
+            
+        }
+
+        #endregion OnGet
+    }
+}
