0b21c54b99f390b6f3d3e1ff917ef42ae176f8b1
emarshall365 <nuri1126@users.noreply.github.com>
2022-05-07 16:36:50 -0700

    Added unit test for About service to AboutUsService.Tests.cs

--- UnitTests/Services/AboutUsService.Tests.cs
+++ UnitTests/Services/AboutUsService.Tests.cs
@@ -1,7 +1,35 @@
-﻿namespace UnitTests.Services
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+namespace UnitTests.Services
 {
     public class AboutUsService_Tests
     {
-        
+        #region TestSetup
+
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+        #region AddRating
+        [Test]
+        public void GetAboutUS_InValid_Data_Model_Should_Return_True()
+        {
+            // Arrange
+
+            // Act
+            //Get data model of about us 
+            var aboutUs = TestHelper.AboutUsServiceObj.GetAboutUs();
+
+            // Assert
+            Assert.AreEqual("LetsGoSEA.WebSite.Models.AboutUsModel[]", aboutUs.ToString());
+        }
+        #endregion AddRating
+
     }
 }
