6510630c960ef9859ac1c8dd66ce4e3c780c4bec
Zi Wang <80916549+zwang4-code@users.noreply.github.com>
2022-05-09 00:56:02 -0700

    Added comments in Update.cshtml.Tests.cs

--- UnitTests/Pages/Neighborhood/Update.cshtml.Tests.cs
+++ UnitTests/Pages/Neighborhood/Update.cshtml.Tests.cs
@@ -6,12 +6,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace UnitTests.Pages.Neighborhood
 {
+    /// <summary>
+    /// Unit test for Update page
+    /// </summary>
     public class UpdateTests
     {
         #region TestSetup
-
+        // UpdateModel object
         private static UpdateModel _pageModel;
 
+        /// <summary>
+        /// Set up Update Model for testing
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -22,9 +28,11 @@ namespace UnitTests.Pages.Neighborhood
 
 
         #region OnGet
-
+        /// <summary>
+        /// Test GET method: valid page should return neighborhoods
+        /// </summary>
         [Test]
-        public void OnGet_Valid_Should_Return_Products()
+        public void OnGet_Valid_Should_Return_Neighborhoods()
         {
             // Arrange
             
@@ -38,9 +46,11 @@ namespace UnitTests.Pages.Neighborhood
         #endregion OnGet
         
         #region OnPostAsync
-
+        /// <summary>
+        /// Test POST method: valid page should return neighborhoods
+        /// </summary>
         [Test]
-        public void OnPostAsync_Valid_Should_Return_Products()
+        public void OnPostAsync_Valid_Should_Return_Neighborhoods()
         {
             // Arrange
             _pageModel.Neighborhood = new NeighborhoodModel
@@ -59,6 +69,9 @@ namespace UnitTests.Pages.Neighborhood
             Assert.AreEqual(true, result.PageName.Contains("Index"));
         }
 
+        /// <summary>
+        /// Test POST method: invalid data should make ModelState invalid
+        /// </summary>
         [Test]
         public void OnPostAsync_InValid_ModelState_Should_Return_False()
         {
