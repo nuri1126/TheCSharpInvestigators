254841c3329e42bc9ec806e32729a8eb23dde7df
Zi Wang <80916549+zwang4-code@users.noreply.github.com>
2022-05-08 18:42:19 -0700

    Added OnPost() function in Create.cshtml.cs

--- src/Pages/Neighborhood/Create.cshtml.cs
+++ src/Pages/Neighborhood/Create.cshtml.cs
@@ -32,6 +32,29 @@ namespace LetsGoSEA.WebSite.Pages.Neighborhood
             // The sole purpose is to show the autopopulated ID, Seattle, and WA fields on the browser
             Neighborhood = NeighborhoodService.CreateID();
         }
-       
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Request user input from the form
+            var ID = Request.Form["Neighborhood.Id"];       // Request.Form returns String[]
+            var ID_int = Int32.Parse(ID[0]);                // Parse ID input into integer 
+            var name = Request.Form["Neighborhood.Name"];
+            var image = Request.Form["Neighborhood.Image"];
+            var shortDesc = Request.Form["Neighborhood.ShortDesc"];
+
+            // Create a new Neighborhood Model object WITH user input
+            // This Neighborhood object is different from the object created in OnGet()
+            // This object will store user input and eventually convert them to JSON
+            Neighborhood = NeighborhoodService.AddData(ID_int, name, image, shortDesc);
+
+            // Redirect to Update page with reference to the new neighborhood
+            return RedirectToPage("./Index");
+        }
+
     }
 }
