1369d787dfa730b06fcbbafd06527252501079d1
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-15 14:25:13 -0700

    Added NeighborhoodController.cs.Tests.cs

---
+++ UnitTests/Controllers/NeighborhoodController.cs.Tests.cs
@@ -0,0 +1,46 @@
+﻿using LetsGoSEA.WebSite.Controllers;
+using NUnit.Framework;
+
+namespace UnitTests.Controllers
+{
+    /// <summary>
+    /// Unit test for Neighborhood Controller
+    /// </summary>
+    class NeighborhoodControllerTests
+    {
+        #region Constructor
+        /// <summary>
+        /// Tests that a NeighborhoodController is created upon instantiation
+        /// </summary>
+        [Test]
+        public void New_Controller_Not_Null()
+        {
+            //Arrange
+
+            //Act
+            var controller = new NeighborhoodController(TestHelper.NeighborhoodServiceObj);
+
+            //Assert
+            Assert.NotNull(controller);
+        }
+        #endregion Constructor
+
+        #region PrivateNeighborhoodService
+        /// <summary>
+        /// Tests the NeighborhoodController's property is not null upon instantiation 
+        /// </summary>
+        [Test]
+        public void NeighborhoodService_Not_Null()
+        {
+            //Arrange
+            var controller = new NeighborhoodController(TestHelper.NeighborhoodServiceObj);
+
+            //Act
+            var service = controller.NeighborhoodService;
+
+            //Assert 
+            Assert.NotNull(service);
+        }
+        #endregion PrivateNeighborhoodService
+    }
+}
