c2989457782c7c141934923fbc76c2aced5ec3e3
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-14 22:49:50 -0700

    Cleaned up using statements and removed extra lines in Delete.cshtml.Tests.cs

--- UnitTests/Pages/Neighborhood/Delete.cshtml.Tests.cs
+++ UnitTests/Pages/Neighborhood/Delete.cshtml.Tests.cs
@@ -1,8 +1,8 @@
-﻿using System.Diagnostics;
-using LetsGoSEA.WebSite.Models;
-using NUnit.Framework;
+﻿using LetsGoSEA.WebSite.Models;
 using LetsGoSEA.WebSite.Pages.Neighborhood;
 using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System.Diagnostics;
 
 namespace UnitTests.Pages.Neighborhood
 {
@@ -35,10 +35,10 @@ namespace UnitTests.Pages.Neighborhood
         public void OnGet_Valid_Should_Return_Neighborhood()
         {
             // Arrange
-            
+
             // Act
             _pageModel.OnGet(2);
-            
+
             // Assert
             Assert.AreEqual(true, _pageModel.ModelState.IsValid);
             Assert.AreEqual("Greenlake", _pageModel.Neighborhood.Name);
@@ -46,7 +46,7 @@ namespace UnitTests.Pages.Neighborhood
 
 
         #endregion OnGet
-        
+
         #region OnPostAsync
         /// <summary>
         /// Test POST method: valid page should be able to delete a record and return neighborhoods 
@@ -59,15 +59,15 @@ namespace UnitTests.Pages.Neighborhood
             _pageModel.Neighborhood.Name = "Neighborhood to Delete";
             _pageModel.Neighborhood.Id = 999;
             TestHelper.NeighborhoodServiceObj.UpdateData(_pageModel.Neighborhood);
-            
+
             // Act
             var result = _pageModel.OnPost() as RedirectToPageResult;
-            
+
             // Assert
             Assert.AreEqual(true, _pageModel.ModelState.IsValid);
             Debug.Assert(result != null, nameof(result) + " != null");
             Assert.AreEqual(true, result.PageName.Contains("Index"));
-            
+
             // Confirm the item is deleted
             Assert.AreEqual(null, TestHelper.NeighborhoodServiceObj.GetNeighborhoodById(_pageModel.Neighborhood.Id));
         }
@@ -85,17 +85,17 @@ namespace UnitTests.Pages.Neighborhood
                 Name = "Invalid",
                 ShortDesc = "Invalid"
             };
-            
+
             // Force an invalid error state
             _pageModel.ModelState.AddModelError("InvalidState", "Neighborhood is invalid");
-            
+
             // Act
             var result = _pageModel.OnPost() as ActionResult;
-            
+
             // Assert
             Assert.AreEqual(false, _pageModel.ModelState.IsValid);
         }
-        
+
         #endregion OnPostAsync
     }
 }
