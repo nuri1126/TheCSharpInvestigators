e64ae08f8b8f458fac96e47085f60f8f5d815a66
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-14 22:50:49 -0700

    Cleaned up using statements and extra lines in Update.cshtml.Tests.cs

--- UnitTests/Pages/Neighborhood/Update.cshtml.Tests.cs
+++ UnitTests/Pages/Neighborhood/Update.cshtml.Tests.cs
@@ -1,8 +1,8 @@
-﻿using System.Diagnostics;
-using LetsGoSEA.WebSite.Models;
-using NUnit.Framework;
+﻿using LetsGoSEA.WebSite.Models;
 using LetsGoSEA.WebSite.Pages.Neighborhood;
 using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System.Diagnostics;
 
 namespace UnitTests.Pages.Neighborhood
 {
@@ -35,16 +35,16 @@ namespace UnitTests.Pages.Neighborhood
         public void OnGet_Valid_Should_Return_Neighborhoods()
         {
             // Arrange
-            
+
             // Act
             _pageModel.OnGet(2);
-            
+
             // Assert
             Assert.AreEqual(true, _pageModel.ModelState.IsValid);
             Assert.AreEqual("Greenlake", _pageModel.Neighborhood.Name);
         }
         #endregion OnGet
-        
+
         #region OnPostAsync
         /// <summary>
         /// Test POST method: valid page should return neighborhoods
@@ -59,10 +59,10 @@ namespace UnitTests.Pages.Neighborhood
                 Name = "Greenwood",
                 ShortDesc = "Welcome to the green neighborhood of Greenwood"
             };
-            
+
             // Act
             var result = _pageModel.OnPost() as RedirectToPageResult;
-            
+
             // Assert
             Assert.AreEqual(true, _pageModel.ModelState.IsValid);
             Debug.Assert(result != null, nameof(result) + " != null");
@@ -82,17 +82,17 @@ namespace UnitTests.Pages.Neighborhood
                 Name = "Invalid Name",
                 ShortDesc = "Invalid Desc"
             };
-            
+
             // Force an invalid error state
             _pageModel.ModelState.AddModelError("InvalidState", "Invalid Neighborhood state");
-            
+
             // Act
             var result = _pageModel.OnPost() as ActionResult;
-            
+
             // Assert
             Assert.AreEqual(false, _pageModel.ModelState.IsValid);
         }
-        
+
         #endregion OnPostAsync
     }
 }
