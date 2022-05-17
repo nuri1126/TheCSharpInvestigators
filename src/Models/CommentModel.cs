056722031c62fc8adaa599196365ec888889234e
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-16 17:14:43 -0700

    Added CommenteModele.cs

---
+++ src/Models/CommentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LetsGoSEA.WebSite.Models
+{
+    /// <summary>
+    /// Comments users submit on a Neighborhood detail page
+    /// </summary>
+    public class CommentModel
+    {
+        // Holds the comment string 
+        public string Comment { get; set; }
+    }
+}
