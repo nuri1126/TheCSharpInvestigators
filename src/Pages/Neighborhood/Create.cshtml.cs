1a0b95e61a39214024484f30bbfadf41aebb6e79
Zi Wang <80916549+zwang4-code@users.noreply.github.com>
2022-05-23 09:02:03 -0700

    Modified OnPost method to catch uploaded image input in Create.cshtml.cs

--- src/Pages/Neighborhood/Create.cshtml.cs
+++ src/Pages/Neighborhood/Create.cshtml.cs
@@ -1,7 +1,10 @@
 ﻿using LetsGoSEA.WebSite.Models;
 using LetsGoSEA.WebSite.Services;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Net;
 
 namespace LetsGoSEA.WebSite.Pages.Neighborhood
 {
@@ -31,16 +34,20 @@ namespace LetsGoSEA.WebSite.Pages.Neighborhood
         /// <returns>Redirect to index page</returns>
         public IActionResult OnPost()
         {
+            // Hold valid image URLs only 
+            List<string> validImageURLs = new List<string>();
 
-            // Request user input from the form
+            // Get user input from the form: name, image link, short description, uploaded files 
             var name = Request.Form["Neighborhood.Name"];
-            var image = Request.Form["Neighborhood.Image"];
+            var imageURLs = Request.Form["Neighborhood.Image"];
             var shortDesc = Request.Form["Neighborhood.ShortDesc"];
+            var imageFiles = Request.Form.Files;
 
+            
             // Create a new Neighborhood Model object WITH user input
             // This Neighborhood object is different from the object created in OnGet()
             // This object will store user input and eventually convert them to JSON
-            Neighborhood = NeighborhoodService.AddData(name, image, shortDesc);
+            Neighborhood = NeighborhoodService.AddData(name, imageURLs, shortDesc, imageFiles);
 
             // Redirect to Update page with reference to the new neighborhood
             return RedirectToPage("./Index");
