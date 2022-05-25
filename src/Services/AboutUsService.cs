29c528a9a475a679e9b070b78719e16d5221fd9f
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-24 21:11:19 -0700

    Applied code standards in AboutUsService.cs

--- src/Services/AboutUsService.cs
+++ src/Services/AboutUsService.cs
@@ -7,29 +7,29 @@ using System.Text.Json;
 namespace LetsGoSEA.WebSite.Services
 {
     /// <summary>
-    /// Mediates communication between a AboutUsController and About Us page details   
+    /// Mediates communication between a AboutUsController and About Us page details.   
     /// </summary>
     public class AboutUsService
     {
-        // Constructor
+        // Constructor.
         public AboutUsService(IWebHostEnvironment webHostEnvironment)
         {
             WebHostEnvironment = webHostEnvironment;
         }
 
-        // Getter: Get JSON file from wwwroot
+        // Getter: Get JSON file from wwwroot.
         private IWebHostEnvironment WebHostEnvironment { get; }
 
-        // Store the path of team member's JSON file (combine the root path, folder name, and file name)
+        // Store the path of team member's JSON file (combine the root path, folder name, and file name).
         private string AboutUsFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "about_us.json");
 
-        // Query to get all the team member objects from JSON file
+        // Query to get all the team member objects from JSON file.
         public IEnumerable<AboutUsModel> GetAboutUs()
         {
             // Open AboutUs JSON file
             using var jsonFileReader = File.OpenText(AboutUsFileName);
 
-            // Read and Deserialize JSON file into an array of team member objects
+            // Read and Deserialize JSON file into an array of team member objects.
             return JsonSerializer.Deserialize<AboutUsModel[]>(jsonFileReader.ReadToEnd(),
                 new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
         }
