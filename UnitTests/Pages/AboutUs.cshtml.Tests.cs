c7e0ebcda7d7244209b530ca0a5cecc6cb4ff56b
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-24 21:13:36 -0700

    Applied code standards in AboutUs.cshtml.Tests.cs

--- UnitTests/Pages/AboutUs.cshtml.Tests.cs
+++ UnitTests/Pages/AboutUs.cshtml.Tests.cs
@@ -5,16 +5,17 @@ using System.Linq;
 namespace UnitTests.Pages
 {
     /// <summary>
-    /// Unit test for About US page
+    /// Unit tests for the About Us Page.
     /// </summary>
     public class AboutUsTests
     {
         #region TestSetup
+
         // AboutUsModel object
         private static LetsGoSEA.WebSite.Pages.AboutUsModel _pageModel;
 
         /// <summary>
-        /// Set up AboutUs Model for testing
+        /// Initialize AboutUsModel for with a NeighborhoodService object. 
         /// </summary>
         [SetUp]
         public void TestInitialize()
@@ -26,7 +27,7 @@ namespace UnitTests.Pages
 
         #region OnGet
         /// <summary>
-        /// Test GET method: valid team member objects should return valid page
+        /// Test that when OnGet is called, valid team member objects are returned to the page. 
         /// </summary>
         [Test]
         public void OnGet_Valid_Members_Should_Return_True()
@@ -38,31 +39,33 @@ namespace UnitTests.Pages
 
             // Assert
             Assert.AreEqual(true, _pageModel.ModelState.IsValid);
-            Assert.AreEqual(true, _pageModel.Members.ToList().Any());
+            Assert.AreEqual(true, _pageModel.members.ToList().Any());
+        }
 
+        /// <summary>
+        /// Tests that when OnGet is called, the properties of each member object are not null.
+        /// </summary>
+        [Test]
+        public void OnGet_Model_Properties_Should_Be_Not_Null()
+        {
+            // Arrange
+
+            // Act
+            _pageModel.OnGet();
+            IEnumerable<LetsGoSEA.WebSite.Models.AboutUsModel> members = _pageModel.members;
+
+            // Assert
+            foreach (var member in members)
+            {
+                Assert.NotNull(member.name);
+                Assert.NotNull(member.bio);
+                Assert.NotNull(member.linkedIn);
+                Assert.NotNull(member.image);
+                Assert.NotNull(member.bio);
+            }
         }
 
         #endregion OnGet
 
-        #region Model_Properties_Should_Be_Not_Null() 
-        [Test]
-        public void Model_Properties_Should_Be_Not_Null()
-        {
-            // Arrange
-            _pageModel.OnGet();
-            IEnumerable<LetsGoSEA.WebSite.Models.AboutUsModel> members = _pageModel.Members;
-
-            // Act
-
-            foreach (var member in members)
-            {
-                Assert.NotNull(member.Name);
-                Assert.NotNull(member.Bio);
-                Assert.NotNull(member.LinkedIn);
-                Assert.NotNull(member.Image);
-                Assert.NotNull(member.Bio);
-            }
-        }
-        #endregion Model_Properties_Should_Be_Not_Null() 
     }
 }
