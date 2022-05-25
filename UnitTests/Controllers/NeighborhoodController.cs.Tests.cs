4a77e951cfee0ea40a2538631b2c9e7a1044af1b
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-24 21:12:24 -0700

    Applied code standards in NeighborhoodController.cs.Tests.cs

--- UnitTests/Controllers/NeighborhoodController.cs.Tests.cs
+++ UnitTests/Controllers/NeighborhoodController.cs.Tests.cs
@@ -4,14 +4,16 @@ using NUnit.Framework;
 namespace UnitTests.Controllers
 {
     /// <summary>
-    /// Unit test for Neighborhood Controller
+    /// Unit test for NeighborhoodController
     /// </summary>
     class NeighborhoodControllerTests
     {
+
         #region Constructor
 
         /// <summary>
-        /// Tests that a NeighborhoodController is created upon instantiation
+        /// Tests that a NeighborhoodController's constructor creates a new NeighborhoodController
+        /// instance when called and valid input parameter is provided.
         /// </summary>
         [Test]
         public void NeighborhoodController_Valid_New_Controller_Not_Null_Should_Return_True()
