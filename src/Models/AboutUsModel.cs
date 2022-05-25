1d01811b3a2c18917cc6b2905083ade9ab409bf6
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-24 21:04:56 -0700

    Applied code standards in AboutUsModel.cs

--- src/Models/AboutUsModel.cs
+++ src/Models/AboutUsModel.cs
@@ -1,20 +1,20 @@
 ﻿namespace LetsGoSEA.WebSite.Models
 {
     /// <summary>
-    /// AboutUsModel represents an individual developer profile
+    /// AboutUsModel represents an individual developer profile.
     /// </summary>
     public class AboutUsModel
     {
-        // Developer name
-        public string Name { get; set; } = "Default";
+        // Developer name.
+        public string name { get; set; } = "Default";
 
-        // LinkedIn web address to developer profile
-        public string LinkedIn { get; set; } = "Default";
+        // LinkedIn web address to developer profile.
+        public string linkedIn { get; set; } = "Default";
 
-        // Developer photo 
-        public string Image { get; set; } = "Default";
+        // Developer photo. 
+        public string image { get; set; } = "Default";
 
-        // Short bio 
-        public string Bio { get; set; } = "Default";
+        // Short bio. 
+        public string bio { get; set; } = "Default";
     }
 }
