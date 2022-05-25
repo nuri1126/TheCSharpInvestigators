1b243f779baa0636079785e1e33c225323562e0a
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-24 21:08:07 -0700

    Applied code standards in Create.cshtml.cs

--- src/Pages/Neighborhood/Create.cshtml.cs
+++ src/Pages/Neighborhood/Create.cshtml.cs
@@ -1,20 +1,17 @@
 ﻿using LetsGoSEA.WebSite.Models;
 using LetsGoSEA.WebSite.Services;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Collections.Generic;
-using System.Net;
 
 namespace LetsGoSEA.WebSite.Pages.Neighborhood
 {
     /// <summary>
-    /// Create Page Model for the Create Razor Page: adds a new Neighborhood to NeighborhoodModel and JSON file
+    /// Create Page Model for the Create Razor Page: adds a new Neighborhood to NeighborhoodModel and JSON file.
     /// </summary>
     public class CreateModel : PageModel
     {
-        // Data middle tier
-        public NeighborhoodService NeighborhoodService { get; }
+        // Data middle tier.
+        public NeighborhoodService neighborhoodService { get; }
 
         /// <summary>
         /// Default Constructor
@@ -22,28 +19,30 @@ namespace LetsGoSEA.WebSite.Pages.Neighborhood
         /// <param name="neighborhoodService">An instance of the data service to use</param>
         public CreateModel(NeighborhoodService neighborhoodService)
         {
-            NeighborhoodService = neighborhoodService;
+            this.neighborhoodService = neighborhoodService;
         }
 
         // The data to show
-        public NeighborhoodModel Neighborhood;
+        public NeighborhoodModel neighborhood;
 
         /// <summary>
-        /// REST Post request: to create a permanent neighborhood object with user input data 
+        /// REST Post request: to create a permanent neighborhood object with user input data. 
         /// </summary>
         /// <returns>Redirect to index page</returns>
         public IActionResult OnPost()
         {
-            // Get user input from the form: name, image link, short description, uploaded files 
+            // Get user input from the form: name, image link, short description, uploaded files.
             var name = Request.Form["Neighborhood.Name"];
             var imageURLs = Request.Form["Neighborhood.Image"];
             var shortDesc = Request.Form["Neighborhood.ShortDesc"];
             var imageFiles = Request.Form.Files;
-            
-            // Create a new Neighborhood Model object WITH user input
-            // This Neighborhood object is different from the object created in OnGet()
-            // This object will store user input and eventually convert them to JSON
-            Neighborhood = NeighborhoodService.AddData(name, imageURLs, shortDesc, imageFiles);
+
+            /*
+             * Create a new Neighborhood Model object WITH user input
+             * This Neighborhood object is different from the object created in OnGet(). This 
+             * object will store user input and eventually convert them to JSON.
+             */
+            neighborhood = neighborhoodService.AddData(name, imageURLs, shortDesc, imageFiles);
 
             // Redirect to Update page with reference to the new neighborhood
             return RedirectToPage("./Index");
