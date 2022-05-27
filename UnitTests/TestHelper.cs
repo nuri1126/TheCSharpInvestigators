30aa2f3d024d1323c86444982bbd535aa3c4da4e
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-26 22:06:40 -0700

    Added comments to TestHelper.cs

--- UnitTests/TestHelper.cs
+++ UnitTests/TestHelper.cs
@@ -12,29 +12,46 @@ using Moq;
 namespace UnitTests
 {
     /// <summary>
-    /// Helper to hold the web start settings
-    ///
-    /// HttpClient
-    /// 
-    /// Action Contect
-    /// 
-    /// The View Data and Teamp Data
-    /// 
-    /// The Product Service
+    /// Test helper to hold the web start settings including:
+    /// HttpClient, Action Context, View Data and Temp Data
+    /// and services
     /// </summary>
     public static class TestHelper
     {
+        // Mock instance IWebHostEnvironment interface.
         public static Mock<IWebHostEnvironment> MockWebHostEnvironment;
+
+        // Test instance of IUrlHelperFactory object to help build URLs.
         public static IUrlHelperFactory UrlHelperFactory;
+
+        // Test instance of HttpContext class that holds properties for Auth, connection, etc.
         public static DefaultHttpContext HttpContextDefault;
+
+        // Test instance of IWebHostEnvironment. 
         public static IWebHostEnvironment WebHostEnvironment;
+
+        // Test instance of a ModelStateDictionary. 
         public static ModelStateDictionary ModelState;
+
+        // Test instance of ActionContext. 
         public static ActionContext ActionContext;
+
+        // Represents an empty model for testing. 
         public static EmptyModelMetadataProvider ModelMetadataProvider;
+
+        // Test ViewDataDictionary object. 
         public static ViewDataDictionary ViewData;
+
+        // Test TempDataDictionary object. 
         public static TempDataDictionary TempData;
+
+        // Test PageContext object.
         public static PageContext PageContext;
+
+        // Test instance of a NeighborhoodService. 
         public static NeighborhoodService NeighborhoodServiceObj;
+
+        // Test instance of the AboutUsService. 
         public static AboutUsService AboutUsServiceObj;
 
         /// <summary>
@@ -42,43 +59,41 @@ namespace UnitTests
         /// </summary>
         static TestHelper()
         {
+            // Initialize and setup MockWebHostEnvironment object. 
             MockWebHostEnvironment = new Mock<IWebHostEnvironment>();
             MockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
             MockWebHostEnvironment.Setup(m => m.WebRootPath).Returns(TestFixture.DataWebRootPath);
             MockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns(TestFixture.DataContentRootPath);
             MockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns(TestFixture.ImageContentRootPath);
 
+            // Initialize and set TraceIdentifier propertiy for HttpContextDefault. 
             HttpContextDefault = new DefaultHttpContext()
             {
                 TraceIdentifier = "trace",
             };
             HttpContextDefault.HttpContext.TraceIdentifier = "trace";
 
+            // Initialize ModelStateDictionary object. 
             ModelState = new ModelStateDictionary();
 
+            // Initialize ActionContext. 
             ActionContext = new ActionContext(HttpContextDefault, HttpContextDefault.GetRouteData(), new PageActionDescriptor(), ModelState);
 
+            // Initialize test Model and associated test data. 
             ModelMetadataProvider = new EmptyModelMetadataProvider();
             ViewData = new ViewDataDictionary(ModelMetadataProvider, ModelState);
             TempData = new TempDataDictionary(HttpContextDefault, Mock.Of<ITempDataProvider>());
 
+            // Initialize PageContext object and set ViewData and HttpContext properties. 
             PageContext = new PageContext(ActionContext)
             {
                 ViewData = ViewData,
                 HttpContext = HttpContextDefault
             };
 
+            // Initialize test services with MockWebHostEnvironment. 
             NeighborhoodServiceObj = new NeighborhoodService(MockWebHostEnvironment.Object);
-
-            /*NeighborhoodService neighborhoodServiceObj;
-
-            neighborhoodServiceObj = new NeighborhoodService(TestHelper.MockWebHostEnvironment.Object);*/
-
             AboutUsServiceObj = new AboutUsService(MockWebHostEnvironment.Object);
-
-            /*AboutUsService aboutUsServiceObj;
-
-            aboutUsServiceObj = new AboutUsService(TestHelper.MockWebHostEnvironment.Object);*/
         }
     }
 }
