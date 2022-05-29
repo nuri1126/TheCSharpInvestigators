47e333f17cb34b94eb4ac4bb0033a212938f33ac
Narissa Tsuboi <narissatsuboi@gmail.com>
2022-05-29 00:07:58 -0700

    Updated tests in Delete.cshtml.Test.cs

--- UnitTests/Pages/Neighborhood/Delete.cshtml.Tests.cs
+++ UnitTests/Pages/Neighborhood/Delete.cshtml.Tests.cs
@@ -1,19 +1,39 @@
 ﻿using LetsGoSEA.WebSite.Models;
 using LetsGoSEA.WebSite.Pages.Neighborhood;
+using LetsGoSEA.WebSite.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using System.Diagnostics;
+using System.Linq;
 
 namespace UnitTests.Pages.Neighborhood
 {
+
     /// <summary>
-    /// Unit test for Delete functionality.
+    /// Unit test for the Delete Page.
     /// </summary>
     public class DeleteTests
     {
+
         #region TestSetup
 
-        // DeleteModel object
+        // Global valid name property for use in tests. 
+        private static readonly string Name = "Bogusland";
+
+        // Global valid image property for use in tests. 
+        private static readonly string Image = "http://via.placeholder.com/150";
+
+        // Global valid shortDesc property for use in tests.
+        private static readonly string ShortDesc = "Test neighborhood description";
+
+        // Global imgFiles property for use in tests. 
+        private static readonly IFormFileCollection ImgFilesNull = null;
+
+        // Global NeighborhodService to use for all test cases. 
+        private NeighborhoodService _neighborhoodService;
+
+        // DeleteModel object.
         private static DeleteModel _pageModel;
 
         /// <summary>
@@ -22,11 +42,14 @@ namespace UnitTests.Pages.Neighborhood
         [SetUp]
         public void TestInitialize()
         {
-            _pageModel = new DeleteModel(TestHelper.NeighborhoodServiceObj);
+            // Abstract NeighborhoodService object from TestHelper.
+            _neighborhoodService = TestHelper.NeighborhoodServiceObj;
+
+            // Initialize pageModel. 
+            _pageModel = new DeleteModel(_neighborhoodService);
         }
 
         #endregion TestSetup
-
 
         #region OnGet
         /// <summary>
@@ -37,12 +60,21 @@ namespace UnitTests.Pages.Neighborhood
         {
             // Arrange
 
+            // Add test neighborhood to database.
+            TestHelper.NeighborhoodServiceObj.AddData(Name, Image, ShortDesc, ImgFilesNull);
+
+            // Retrieve test neighborhood.
+            var testNeighborhood = _neighborhoodService.GetNeighborhoods().Last();
+
             // Act
-            _pageModel.OnGet(2);
+            _pageModel.OnGet(testNeighborhood.id);
 
             // Assert
             Assert.AreEqual(true, _pageModel.ModelState.IsValid);
-            Assert.AreEqual("Greenlake", _pageModel.neighborhood.name);
+            Assert.AreEqual(testNeighborhood.name, _pageModel.neighborhood.name);
+
+            // TearDown
+            TestHelper.NeighborhoodServiceObj.DeleteData(testNeighborhood.id);
         }
 
         #endregion OnGet
@@ -55,10 +87,10 @@ namespace UnitTests.Pages.Neighborhood
         public void OnPost_Delete_Valid_Neighborhood_Should_Return_Neighborhoods()
         {
             // Create neighborhood object to delete. 
-            _pageModel.neighborhood = TestHelper.NeighborhoodServiceObj.CreateID();
-            _pageModel.neighborhood.name = "Neighborhood to Delete";
+            _pageModel.neighborhood = _neighborhoodService.CreateID();
+            _pageModel.neighborhood.name = Name;
             _pageModel.neighborhood.id = 999;
-            TestHelper.NeighborhoodServiceObj.UpdateData(_pageModel.neighborhood);
+            _neighborhoodService.UpdateData(_pageModel.neighborhood);
 
             // Act
             var result = _pageModel.OnPost() as RedirectToPageResult;
@@ -70,6 +102,7 @@ namespace UnitTests.Pages.Neighborhood
 
             // Confirm the item is deleted.
             Assert.AreEqual(null, TestHelper.NeighborhoodServiceObj.GetNeighborhoodById(_pageModel.neighborhood.id));
+
         }
 
         /// <summary>
@@ -77,14 +110,14 @@ namespace UnitTests.Pages.Neighborhood
         /// becomes invalid. 
         /// </summary>
         [Test]
-        public void OnPost_Invalid_Model_NotValid_ReturnPage()
+        public void OnPost_Invalid_Model_NotValid_Should_Return_False()
         {
             // Arrange
             _pageModel.neighborhood = new NeighborhoodModel
             {
                 id = 999,
-                name = "Invalid",
-                shortDesc = "Invalid"
+                name = Name,
+                shortDesc = ShortDesc
             };
 
             // Force an invalid error state.
