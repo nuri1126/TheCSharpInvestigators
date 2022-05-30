2ff8c1a8bca2118891aa6022aaa09de18d16e58f
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2022-05-30 04:13:36 -0700

    Adding Address property to Read.cshtml.Tests.cs

--- UnitTests/Pages/Neighborhood/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Neighborhood/Read.cshtml.Tests.cs
@@ -20,14 +20,17 @@ namespace UnitTests.Pages.Neighborhood
 
         // Global valid image property for use in tests. 
         private static readonly string Image = "http://via.placeholder.com/150";
+        
+        // Global valid address property for use in tests
+        private static readonly string Address = "401 NE Northgate Way, Seattle, WA 98125";
 
         // Global valid shortDesc property for use in tests.
         private static readonly string ShortDesc = "Test neighborhood description";
 
         // Global imgFiles property for use in tests. 
-        private static IFormFileCollection ImgFilesNull = null;
+        private static readonly IFormFileCollection ImgFilesNull = null;
 
-        // Global NeighborhodService to use for all test cases. 
+        // Global NeighborhoodService to use for all test cases. 
         private NeighborhoodService _neighborhoodService;
 
         // ReadModel object.
@@ -59,7 +62,7 @@ namespace UnitTests.Pages.Neighborhood
             // Arrange
 
             // Add test neighborhood to database.
-            _neighborhoodService.AddData(Name, Image, ShortDesc, ImgFilesNull);
+            _neighborhoodService.AddData(Name, Address, Image, ShortDesc, ImgFilesNull);
 
             // Retrieve test neighborhood.
             var testNeighborhood = _neighborhoodService.GetNeighborhoods().Last();
@@ -86,7 +89,7 @@ namespace UnitTests.Pages.Neighborhood
 
             // Arrange
 
-            _pageModel.currentNeighborhood = new NeighborhoodModel
+            _pageModel.neighborhood = new NeighborhoodModel
             {
                 id = 999,
                 name = Name,
@@ -96,7 +99,7 @@ namespace UnitTests.Pages.Neighborhood
             _pageModel.ModelState.AddModelError("InvalidState", "Neighborhood is invalid");
 
             // Act
-            var result = _pageModel.OnGet(_pageModel.currentNeighborhood.id);
+            var result = _pageModel.OnGet(_pageModel.neighborhood.id);
 
             // Assert
             Assert.AreEqual(false, _pageModel.ModelState.IsValid);
