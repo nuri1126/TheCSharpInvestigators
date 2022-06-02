b7b74fef68d0755ce5d16745796f4529dce5dee5
Zi Wang <80916549+zwang4-code@users.noreply.github.com>
2022-06-02 09:22:14 -0700

    Created a UploadedImageModel.cs file in Models folder

---
+++ src/Models/UploadedImageModel.cs
@@ -0,0 +1,15 @@
+﻿namespace LetsGoSEA.WebSite.Models
+{
+    public class UploadedImageModel
+    {
+        // Unique identifier
+        public string UploadedImageId { get; set; } = "Default";
+
+        // Holds the uploaded image name
+        public string UploadedImageName { get; set; } = "Default";
+
+        // Holds the uploaded image path
+        public string UploadedImagePath { get; set; } = "Default";
+
+    }
+}
